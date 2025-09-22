fcaa10a4a023ad994da0a033673e2cfcf0c6cf68
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-09-22 17:23:40 +0200

    adding to dbcontext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,6 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Rental> Rentals { get; set; }
+
 }
