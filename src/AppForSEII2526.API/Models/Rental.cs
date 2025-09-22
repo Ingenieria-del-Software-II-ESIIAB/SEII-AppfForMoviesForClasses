89f4516d5c98d894dd9b721d3d3e167382bb3be0
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-09-22 17:19:54 +0200

    adding atttributes title and cost to rental

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.Models {
+    public class Rental {
+        public int Id { get; set; }
+
+        [StringLength(20,
+            ErrorMessage ="Maximum 20, minimum 10",
+            MinimumLength =10)]
+        public string Title { get; set; }
+
+        [Precision(10,2)]
+        public decimal CostofRental { get; set; }
+    }
+}
