e5f21496c4d7bbf62239a5fc32ec35f41add5b25
Elena Navarro <elena.navarro@uclm.es>
2025-09-29 12:47:40 +0200

    Task2 add rental class to model folder (#6)
    closes: #2
    * adding atttributes title and cost to rental
    
    * adding to dbcontext
    
    * adding RentalItem
    
    ---------
    
    Co-authored-by: Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,5 +6,8 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<Rental> Rentals { get; set; }
+    public DbSet<RentalItem> RentalItems { get; set; }
+    
+
 
 }
---
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -0,0 +1,12 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class RentalItem
+    {
+        public int Id { get; set; }
+        public Rental Rental { get; set; }
+
+        [Precision(10,2)]
+        public decimal Price { get; set; }
+  
+    }
+}
