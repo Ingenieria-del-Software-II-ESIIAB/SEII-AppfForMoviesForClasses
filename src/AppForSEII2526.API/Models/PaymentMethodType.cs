2ed7f140d4d24e7b284c3e9f49d1151ec1e788a5
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-10-03 18:39:03 +0200

    establishing the relationship between Rental and RentalItems

---
+++ src/AppForSEII2526.API/Models/PaymentMethodType.cs
@@ -0,0 +1,7 @@
+﻿namespace AppForSEII2526.API.Models {
+    public enum PaymentMethodTypes {
+        CreditCard,
+        PayPal,
+        Cash
+    }
+}
--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -2,14 +2,30 @@
     public class Rental {
         public int Id { get; set; }
 
-        [StringLength(20,
-            ErrorMessage ="Maximum 20, minimum 10",
-            MinimumLength =10)]
-        public string Title { get; set; }
+        [StringLength(20,ErrorMessage ="Maximum 50, minimum 10",MinimumLength =10)]
+        public string DeliveryAddress { get; set; }
+
+        [StringLength(20,ErrorMessage = "Maximum 50, minimum 10",MinimumLength = 10)]
+        public string NameCustomer { get; set; }
+
+        [StringLength(20,ErrorMessage = "Maximum 50, minimum 10",MinimumLength = 10)]
+        public string SurnameCustomer { get; set; }
 
         [Precision(10,2)]
         public decimal CostofRental { get; set; }
 
         public IList<RentalItem> RentalItems { get; set; }
+
+        public DateTime RentalDate { get; set; }
+
+        public DateTime RentalDateFrom { get; set; }
+        public DateTime RentalDateTo { get; set; }
+
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+
+
     }
+
+
 }
