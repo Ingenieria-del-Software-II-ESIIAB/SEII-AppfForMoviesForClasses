fc9265a9f54e1704e0707eaeb96f65db5702e81d
Elena Navarro <elena.navarro@uclm.es>
2025-10-03 19:11:00 +0200

    Task15 developing services for quering the movie table (#19)
    closes: #15
    * devloping the service
    
    * adding movie
    
    * relating Movie to RentalItem
    
    * establishing the relationship between RentalItem and Movie
    
    * establishing the relationship between Rental and RentalItems
    
    * establishing the relationship from Rental to ApplicaitonUser
    
    * adding attributes to ApplicationUser
    
    * establishing the relations from Rental to ApplicationUser
    
    * an alternative for the relationship with customer, and avoid that if an ApplicationUser is deleted, its rental was also deleted
    
    * establishing Title as unique, and avoiding that if a Genre is deleted, its related movies were deleted as well
    
    ---------
    
    Co-authored-by: Elena.Navarro <Elena.Navarro@UCLMEDU61497>

---
+++ src/AppForSEII2526.API/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿namespace AppForSEII2526.API.Models {
+    [Index(nameof(Title),IsUnique=true)]
+    public class Movie {
+
+        public int Id { get; set; }
+
+
+        [StringLength(50, ErrorMessage = "Title name cannot be longer than 50 characters.")]
+        public string Title { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Price For Purchase")]
+        [Precision(10, 2)]
+        public decimal PriceForPurchase { get; set; }
+
+        [Display(Name = "Quantity For Purchase")]
+        [Range(0, int.MaxValue, ErrorMessage = "Minimum quantity for Purchase is 1")]
+        public int QuantityForPurchase { get; set; }
+
+        //required to avoid an OnCascade delete
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public Genre Genre { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Price For Renting")]
+        [Precision(10, 2)]
+        public decimal PriceForRenting { get; set; }
+
+        [Display(Name = "Quantity For Renting")]
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for renting is 1")]
+        public int QuantityForRental { get; set; }
+
+        public IList<RentalItem> RentalItems { get; set; }
+    }
+}
--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -2,14 +2,40 @@
     public class Rental {
         public int Id { get; set; }
 
-        [StringLength(20,
-            ErrorMessage ="Maximum 20, minimum 10",
-            MinimumLength =10)]
-        public string Title { get; set; }
+        [StringLength(20,ErrorMessage ="Maximum 50, minimum 10",MinimumLength =10)]
+        public string DeliveryAddress { get; set; }
+
+        [StringLength(20,ErrorMessage = "Maximum 50, minimum 10",MinimumLength = 10)]
+        public string NameCustomer { get; set; }
+
+        [StringLength(20,ErrorMessage = "Maximum 50, minimum 10",MinimumLength = 10)]
+        public string SurnameCustomer { get; set; }
 
         [Precision(10,2)]
         public decimal CostofRental { get; set; }
 
         public IList<RentalItem> RentalItems { get; set; }
+
+        public DateTime RentalDate { get; set; }
+
+        public DateTime RentalDateFrom { get; set; }
+        public DateTime RentalDateTo { get; set; }
+
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+
+        //if the Customer is deleted the related Rental won't be deleted
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public ApplicationUser Customer { get; set; }
+
+
+        //if we want to specify the foreign key
+        //public string CustomerId { get; set; }
+
+        //[ForeignKey(nameof(CustomerId))]
+        //public ApplicationUser Customer { get; set; }
+
     }
+
+
 }
