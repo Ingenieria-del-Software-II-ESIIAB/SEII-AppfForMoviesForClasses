1b58f74450a4b8105b9e93f5bf401b2399f5c33a
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-10-03 18:44:38 +0200

    adding attributes to ApplicationUser

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,16 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+
+    [Required]
+    public string Name {get;set;}
+
+    [Required]
+    public string Surname {get;set;}
+
+    [Required]
+    public string Address { get; set; }
+
+
+
 }
