88e98e95611940a1513190bcf73e3308ddeb2612
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-10-03 18:32:13 +0200

    establishing the relationship between RentalItem and Movie

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -1,12 +1,24 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(RentalId), nameof(MovieId))]
     public class RentalItem
     {
-        public int Id { get; set; }
+        public int RentalId { get; set; }
+
+        [ForeignKey(nameof(RentalId))]
         public Rental Rental { get; set; }
 
         [Precision(10,2)]
         public decimal Price { get; set; }
-  
+
+        public int MovieId { get; set; }
+
+        [ForeignKey(nameof(MovieId))]
+        public Movie Movie { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "Description cannot be longer than 100 characters.")]
+        public string? Description { get; set; }
+
     }
 }
