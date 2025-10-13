67e50643a4603cecdaeb420994b33e0a1475e702
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-10-13 17:33:13 +0200

    creating script

--- src/AppForSEII2526.API/Controllers/MoviesController.cs
+++ src/AppForSEII2526.API/Controllers/MoviesController.cs
@@ -17,17 +17,18 @@ namespace AppForSEII2526.API.Controllers
         }
         //[HttpGet]
         //[Route("[action]")]
-        //[ProducesResponseType(typeof(decimal), (int) HttpStatusCode.OK)]
-        //[ProducesResponseType(typeof(string), (int) HttpStatusCode.BadRequest)]
-        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2) {
+        //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        //{
 
         //    if (op2 == 0)
         //    {
         //        string error = "Op2 cannot be 0 to compute a division";
-        //        _logger.LogError(DateTime.Now + " Error:"+ error);
+        //        _logger.LogError(DateTime.Now + " Error:" + error);
         //        return BadRequest(error);
         //    }
-        //    decimal result = op1/op2;
+        //    decimal result = op1 / op2;
         //    return Ok(result);
         //}
 
---
+++ src/AppForSEII2526.API/Data/dbo.Movies.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Genre] ON
+INSERT INTO [dbo].[Genre] ([Id], [Name]) VALUES (1, N'Comedy')
+INSERT INTO [dbo].[Genre] ([Id], [Name]) VALUES (2, N'Drama')
+SET IDENTITY_INSERT [dbo].[Genre] OFF
+
+SET IDENTITY_INSERT [dbo].[Movies] ON
+INSERT INTO [dbo].[Movies] ([Id], [Title], [PriceForPurchase], [QuantityForPurchase], [GenreId], [ReleaseDate], [PriceForRenting], [QuantityForRental]) VALUES (1, N'The man in the high castle', CAST(10.00 AS Decimal(10, 2)), 100, 2, N'2020-01-01 00:00:00', CAST(1.00 AS Decimal(10, 2)), 100)
+SET IDENTITY_INSERT [dbo].[Movies] OFF
