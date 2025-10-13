c888c265fec574e2bac2af0392d70242e18ef5c0
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-10-13 17:10:50 +0200

    creating the controller

---
+++ src/AppForSEII2526.API/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<MoviesController> _logger;
+
+        public MoviesController(ApplicationDbContext context, ILogger<MoviesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(decimal), (int) HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int) HttpStatusCode.BadRequest)]
+        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2) {
+
+        //    if (op2 == 0)
+        //    {
+        //        string error = "Op2 cannot be 0 to compute a division";
+        //        _logger.LogError(DateTime.Now + " Error:"+ error);
+        //        return BadRequest(error);
+        //    }
+        //    decimal result = op1/op2;
+        //    return Ok(result);
+        //}
+
+
+    }
+}
