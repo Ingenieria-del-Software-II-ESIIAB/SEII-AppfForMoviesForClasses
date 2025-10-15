a479be287bfc32df9a11c595114d5060db9ecc10
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-10-15 14:08:39 +0200

    Creating DTOs for Rental for GetRental

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace AppForSEII2526.API.DTOs.RentalDTOs {
+    public class RentalItemDTO {
+        public RentalItemDTO(int movieID, string title, string genre, decimal priceForRenting, string? description) {
+            MovieID = movieID;
+            Title = title;
+            PriceForRenting = priceForRenting;
+            Description = description;
+            Genre = genre;
+        }
+
+        public int MovieID { get; set; }
+
+
+        public string Title { get; set; }
+
+        [Precision(10, 2)]
+        public decimal PriceForRenting { get; set; }
+
+        public string? Description { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
