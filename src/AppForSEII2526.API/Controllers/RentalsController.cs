824cd82f93837f8c497d448607bbe51495ab097f
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-10-15 14:11:43 +0200

    creating the Controller for Rental and its constructor

---
+++ src/AppForSEII2526.API/Controllers/RentalsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase {
+        private ApplicationDbContext _context;
+        private ILogger<RentalsController> _logger;
+
+        public RentalsController(ApplicationDbContext context, ILogger<RentalsController> logger) {
+            _context = context;
+            _logger = logger;
+        }
+    }
+}
