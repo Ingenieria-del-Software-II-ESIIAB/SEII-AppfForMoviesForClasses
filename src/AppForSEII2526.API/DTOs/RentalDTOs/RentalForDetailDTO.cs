104903ee3d95c9fcf1b6a511cfecdcd4e7cc126a
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-10-15 17:54:48 +0200

    simplifying the implementation of RentalForDetailDTO

--- src/AppForSEII2526.API/DTOs/RentalDTOs/RentalForDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalForDetailDTO.cs
@@ -1,18 +1,50 @@
 ﻿using AppForSEII2526.API.Models;
 
 namespace AppForSEII2526.API.DTOs.RentalDTOs {
-    public class RentalForDetailDTO:RentalForCreateDTO {
+    public class RentalForDetailDTO {
         public RentalForDetailDTO(int id, decimal costofRental, DateTime rentalDate,string deliveryAddress, 
-            string nameCustomer, string surnameCustomer, 
-            DateTime rentalDateFrom, DateTime rentalDateTo, PaymentMethodType paymentMethod, 
-            string userNameCustomer, IList<RentalItemDTO> rentalItems)
-                :base(deliveryAddress, nameCustomer, surnameCustomer, rentalDateFrom, rentalDateTo, paymentMethod, userNameCustomer, rentalItems) {
+                string nameCustomer, string surnameCustomer, 
+                DateTime rentalDateFrom, DateTime rentalDateTo, PaymentMethodType paymentMethod, 
+                string userNameCustomer, IList<RentalItemDTO> rentalItems) {
+
             Id = id;
             CostofRental = costofRental;
             RentalDate = rentalDate;
+            DeliveryAddress = deliveryAddress;
+            NameCustomer = nameCustomer;
+            SurnameCustomer = surnameCustomer;
+            RentalItems = rentalItems;
+            RentalDateFrom = rentalDateFrom;
+            RentalDateTo = rentalDateTo;
+            PaymentMethod = paymentMethod;
+            UserNameCustomer = userNameCustomer;
+            RentalItems = rentalItems;
         }
 
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "Delivery address must have at least 10 characters")]
+        public string DeliveryAddress { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must have at least 3 characters")]
+        public string NameCustomer { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Surname must have at least 3 characters")]
+        public string SurnameCustomer { get; set; }
+
+        public IList<RentalItemDTO> RentalItems { get; set; }
+
+        public DateTime RentalDateFrom { get; set; }
+
+        public DateTime RentalDateTo { get; set; }
+
+
+        [Required]
+        public PaymentMethodType PaymentMethod { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string UserNameCustomer { get; set; }
 
         [Precision(10, 2)]
         public decimal CostofRental { get; set; }
@@ -21,7 +53,7 @@ namespace AppForSEII2526.API.DTOs.RentalDTOs {
 
         public override bool Equals(object? obj) {
             return obj is RentalForDetailDTO dTO &&
-                   base.Equals(obj) &&
+                   Id == dTO.Id &&
                    DeliveryAddress == dTO.DeliveryAddress &&
                    NameCustomer == dTO.NameCustomer &&
                    SurnameCustomer == dTO.SurnameCustomer &&
@@ -32,7 +64,6 @@ namespace AppForSEII2526.API.DTOs.RentalDTOs {
                    RentalDateTo == dTO.RentalDateTo &&
                    PaymentMethod == dTO.PaymentMethod &&
                    UserNameCustomer == dTO.UserNameCustomer &&
-                   Id == dTO.Id &&
                    CostofRental == dTO.CostofRental &&
                    RentalDate == dTO.RentalDate;
         }
