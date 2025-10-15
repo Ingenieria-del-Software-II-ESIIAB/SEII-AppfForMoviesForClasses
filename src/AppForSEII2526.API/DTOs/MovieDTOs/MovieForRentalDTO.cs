7a053ff1ed473e4902bfe3981a403ba444d584f3
Elena Navarro <elena.navarro@uclm.es>
2025-10-15 13:04:21 +0200

    Task24 develop selectmoviesforrenting (#28)
    closes: #24
    
    * creating the controller
    
    * creating script
    
    * developing selectmovies dto creado
    
    * adding another filter
    
    * how to obtain when the movie was last rented
    
    * how to create Users automatically in the database whenever the Web API  is run
    
    * how to show only those movies that are available for renting duing such period
    
    ---------
    
    Co-authored-by: Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
    Co-authored-by: Elena.Navarro <Elena.Navarro@UCLMEDU61497>

---
+++ src/AppForSEII2526.API/DTOs/MovieDTOs/MovieForRentalDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace AppForSEII2526.API.DTOs.MovieDTOs {
+    public class MovieForRentalDTO {
+        public MovieForRentalDTO(int id, string title, string genreName, DateTime releaseDate, decimal priceForRenting, DateTime? lastRental) {
+            Id = id;
+            Title = title;
+            Genre= genreName;
+            ReleaseDate = releaseDate;
+            PriceForRenting = priceForRenting;
+            LastRental = lastRental;
+        }
+
+        public int Id { get; set; }
+
+
+        [StringLength(50, ErrorMessage = "Title name cannot be longer than 50 characters.")]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Precision(10, 2)]
+        public decimal PriceForRenting { get; set; } 
+
+        public DateTime? LastRental { get; set; }
+
+    }
+}
