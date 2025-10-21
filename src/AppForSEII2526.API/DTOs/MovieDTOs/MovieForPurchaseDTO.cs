01cab92e658e17f10dacfd8ccea8316f7323532c
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-10-21 11:45:34 +0200

    creating MovieForPurchaseDTO

---
+++ src/AppForSEII2526.API/DTOs/MovieDTOs/MovieForPurchaseDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace AppForSEII2526.API.DTOs.MovieDTOs {
+    public class MovieForPurchaseDTO {
+
+        public int Id { get; set; }
+
+
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        //it assigns a value by default
+        public string Title { get; set; } = string.Empty;
+
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+
+
+        [Range(1, float.MaxValue, ErrorMessage = "Minimum price is 1 ")]
+        [Precision(10, 2)]
+        public decimal PriceForPurchase { get; set; }
+
+
+
+
+    }
+}
