af447b49b16c114fcebafbef9ac3377aee4ab811
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-10-27 19:31:53 +0100

    developing GetMoviesForPurchase_NULL_TITLE_GENRE

--- test/AppForSEII2526.UT/MoviesController_test/GetMoviesForPurchase_test.cs
+++ test/AppForSEII2526.UT/MoviesController_test/GetMoviesForPurchase_test.cs
@@ -1,4 +1,6 @@
 ﻿using AppForMovies.UT;
+using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs.MovieDTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +19,36 @@ namespace AppForSEII2526.UT.MoviesController_test {
             var movies = new List<Movie>(){
                     new Movie("The lord of the rings", genres[0],new DateTime(2011, 10, 20),10.0m, 5,1.0m,1),
                     new Movie("The mechanic orange", genres[1],new DateTime(1988, 02, 23),15.0m, 10,2.0m,2),
+
              //this movie has quantityforpurchase=0 and quantityforrenting=0 so it shouldn't be returned when 
                     new Movie("The flying castle", genres[1],new DateTime(2007, 04, 04),20.0m, 0,3.0m,10),
             };
             _context.AddRange(genres);
             _context.AddRange(movies);
             _context.SaveChanges();
+
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Unit Testing")]
+        public async Task GetMoviesForPurchase_NULL_TITLE_GENRE() {
+            // Arrange
+            var expectedMovies = new List<MovieForPurchaseDTO>() {
+                new MovieForPurchaseDTO(1,"The lord of the rings","Sci - Fi",new DateTime(2011, 10, 20),10.0m),
+                new MovieForPurchaseDTO(2,"The mechanic orange","Drama" , new DateTime(1988, 02, 23),15.0m),
+            };
+
+            var controller = new MoviesController(_context, null);
+
+            // Act
+            var result = await controller.GetMoviesForPurchase(null, null);
+
+            //Assert
+            //we check that the response type is OK 
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            //and obtain the list of movies
+            var movieDTOsActual = Assert.IsType<List<MovieForPurchaseDTO>>(okResult.Value);
+            Assert.Equal(expectedMovies, movieDTOsActual);
 
         }
     }
