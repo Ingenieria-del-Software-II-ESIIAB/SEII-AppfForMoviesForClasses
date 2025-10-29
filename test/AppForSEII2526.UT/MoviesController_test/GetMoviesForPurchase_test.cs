c72bd690146149ce5b296341dce7c158c57e7b4e
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-10-29 14:28:43 +0100

    developing theory for GetMovies

--- test/AppForSEII2526.UT/MoviesController_test/GetMoviesForPurchase_test.cs
+++ test/AppForSEII2526.UT/MoviesController_test/GetMoviesForPurchase_test.cs
@@ -1,11 +1,6 @@
 ﻿using AppForMovies.UT;
 using AppForSEII2526.API.Controllers;
 using AppForSEII2526.API.DTOs.MovieDTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppForSEII2526.UT.MoviesController_test {
     public class GetMoviesForPurchase_test : AppForMovies4SqliteUT {
@@ -42,6 +37,56 @@ namespace AppForSEII2526.UT.MoviesController_test {
 
             // Act
             var result = await controller.GetMoviesForPurchase(null, null);
+
+            //Assert
+            //we check that the response type is OK 
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            //and obtain the list of movies
+            var movieDTOsActual = Assert.IsType<List<MovieForPurchaseDTO>>(okResult.Value);
+            Assert.Equal(expectedMovies, movieDTOsActual);
+
+        }
+
+        public static IEnumerable<object[]> TestCasesFor_GetMoviesForPurchase_OK() {
+
+            var movieDTOs = new List<MovieForPurchaseDTO>() {
+                new MovieForPurchaseDTO(1,"The lord of the rings","Sci - Fi",new DateTime(2011, 10, 20),10.0m),
+                new MovieForPurchaseDTO(2,"The mechanic orange","Drama" , new DateTime(1988, 02, 23),15.0m),
+
+            };
+
+            var movieDTOsTC1 = new List<MovieForPurchaseDTO>() { movieDTOs[0], movieDTOs[1] };
+                    //the GetMoviesForPurchase method returns the movies ordered by title
+
+
+
+            var movieDTOsTC2 = new List<MovieForPurchaseDTO>() { movieDTOs[0] };
+            var movieDTOsTC3 = new List<MovieForPurchaseDTO>() { movieDTOs[1] };
+
+
+
+            var allTests = new List<object[]>
+            {             //filters to apply - expected movies
+                                          //by default datefrom=today +1, dateto=today+2, thus movieDTOs[0] cannot be returned
+                new object[] { null, null, movieDTOsTC1,  },
+                new object[] { "lord", null, movieDTOsTC2, },
+                new object[] { null, "Drama", movieDTOsTC3, },
+
+            };
+
+            return allTests;
+        }
+
+        [Theory]
+        [Trait("LevelTesting", "Unit Testing")]
+        [MemberData(nameof(TestCasesFor_GetMoviesForPurchase_OK))]
+        public async Task GetMoviesForPurchase_filter_test(string? movieTitle, string? movieGenre, List<MovieForPurchaseDTO> expectedMovies) {
+            // Arrange
+
+            var controller = new MoviesController(_context, null);
+
+            // Act
+            var result = await controller.GetMoviesForPurchase(movieTitle, movieGenre);
 
             //Assert
             //we check that the response type is OK 
