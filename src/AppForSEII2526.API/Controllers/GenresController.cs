80f2eeb85ce054626dd09876f3bf5bcdd321a2cf
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-11-24 19:35:03 +0100

    creating seeding and GenresController

---
+++ src/AppForSEII2526.API/Controllers/GenresController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers {
+    public class GenresController : Controller {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        public GenresController(ApplicationDbContext context, ILogger<MoviesController> logger) {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Movies/GetMoviesForPurchase
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetGenres(string? genreName) {
+
+            IList<string> genres = await _context.Genre
+                .Where(genre => (genreName == null || genre.Name.Contains(genreName))) // where clause             
+                .OrderBy(genre => genre.Name)
+                .Select(genre => genre.Name)
+                .ToListAsync();
+
+            return Ok(genres);
+        }
+    }
+}
