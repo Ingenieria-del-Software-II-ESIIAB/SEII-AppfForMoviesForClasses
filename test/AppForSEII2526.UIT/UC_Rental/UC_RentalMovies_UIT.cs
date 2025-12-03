6b2d95f24399995d4cdc614cfc6250f19b855587
Elena.Navarro <Elena.Navarro@UCLMEDU61497>
2025-12-03 10:37:23 +0100

    we create the class for implementing the UIT test cases

---
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_Rental {
+    public class UC_RentalMovies_UIT : UC_UIT {
+        public UC_RentalMovies_UIT(ITestOutputHelper output) : base(output) {
+        }
+    }
+}
