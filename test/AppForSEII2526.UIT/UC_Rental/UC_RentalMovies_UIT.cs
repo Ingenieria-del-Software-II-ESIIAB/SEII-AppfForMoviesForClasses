c8b3d3aa0dd5e3cee93092101bec3d56e6818d2b
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-12-03 12:58:43 +0100

    we add the id to the option of the menue we want to select we support the intial steps: login, and click on rental

--- test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
@@ -7,13 +7,21 @@ using AppForSEII2526.UIT.Shared;
 
 namespace AppForSEII2526.UIT.UC_Rental {
     public class UC_RentalMovies_UIT : UC_UIT {
-        
+        private SelectMoviesForRental_PO selectMoviesForRental_PO;
 
         public UC_RentalMovies_UIT(ITestOutputHelper output) : base(output) {
         }
 
         private void Precondition_perform_login() {
             Perform_login("elena@uclm.es", "Password1234%");
+        }
+
+        private void InitialStepsForRentalMovies() { 
+            Precondition_perform_login();
+            //we wait for the option of the menu to be visible
+            selectMoviesForRental_PO.WaitForBeingVisible(By.Id("CreateRental"));
+            //we click on the menu
+            _driver.FindElement(By.Id("CreateRental")).Click();
         }
 
 
