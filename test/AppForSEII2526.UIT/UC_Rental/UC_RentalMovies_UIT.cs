7d54a06ba700df7706ca85bf4c016a6bf0ef1e9a
Elena Navarro <elena.navarro@uclm.es>
2025-12-03 15:02:31 +0100

    we develop the first filter: filter by title

--- test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
@@ -8,8 +8,18 @@ using AppForSEII2526.UIT.Shared;
 namespace AppForSEII2526.UIT.UC_Rental {
     public class UC_RentalMovies_UIT : UC_UIT {
         private SelectMoviesForRental_PO selectMoviesForRental_PO;
+        private const int movieId1 = 1;
+        private const string movieTitle1 = "The man in the high castle";
+        private const string movieGenre1 = "Sci - Fi";
+        private const string moviePriceForRenting1 = "4";
+        private const string movieReleaseDate1 = "1/1/2015";
+
+
+
 
         public UC_RentalMovies_UIT(ITestOutputHelper output) : base(output) {
+
+            selectMoviesForRental_PO = new SelectMoviesForRental_PO(_driver, _output);
         }
 
         private void Precondition_perform_login() {
@@ -23,6 +33,23 @@ namespace AppForSEII2526.UIT.UC_Rental {
             //we click on the menu
             _driver.FindElement(By.Id("CreateRental")).Click();
         }
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_AF1_UC2_4_5_6_filtering()
+        {
+            //Arrange
+            InitialStepsForRentalMovies();
+            var expectedMovies = new List<string[]> { new string[] { movieTitle1, movieGenre1, movieReleaseDate1, moviePriceForRenting1 },};
+
+            //Act
+            selectMoviesForRental_PO.SearchMovies("Last of", "", "", "");
+
+            //Assert
+
+            Assert.True(selectMoviesForRental_PO.CheckListOfMovies(expectedMovies));
+
+        }
+
 
 
     }
