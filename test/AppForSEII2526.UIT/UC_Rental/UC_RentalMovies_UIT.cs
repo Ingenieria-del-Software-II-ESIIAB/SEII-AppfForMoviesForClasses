af9a8e856fa71663d5f4acab7ee7d3c2da649612
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-12-03 12:53:13 +0100

    we support the precondition of our UseCase Login

--- test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalMovies_UIT.cs
@@ -7,7 +7,15 @@ using AppForSEII2526.UIT.Shared;
 
 namespace AppForSEII2526.UIT.UC_Rental {
     public class UC_RentalMovies_UIT : UC_UIT {
+        
+
         public UC_RentalMovies_UIT(ITestOutputHelper output) : base(output) {
         }
+
+        private void Precondition_perform_login() {
+            Perform_login("elena@uclm.es", "Password1234%");
+        }
+
+
     }
 }
