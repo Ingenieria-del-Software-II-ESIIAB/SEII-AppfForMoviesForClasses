da1f81e53fc11fc87777bdb5c25ae14b34bc44bf
Elena.Navarro <Elena.Navarro@UCLMEDU61497>
2025-12-03 10:59:02 +0100

    creating the PageObject to interact with SelectMoviesForRental. Adding locators. Interacting with the Interface to search for movies

---
+++ test/AppForSEII2526.UIT/UC_Rental/SelectMoviesForRental_PO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Rental {
+    public class SelectMoviesForRental_PO : PageObject {
+        By inputTitle = By.Id("movieTitle");
+        By inputGenre = By.Id("selectGenre");
+        By buttonSearchMovies = By.Id("searchMovies");
+        public SelectMoviesForRental_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) {
+        }
+        public void SearchMovies(string title) {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputTitle);
+            _driver.FindElement(inputTitle).SendKeys(title);
+            _driver.FindElement(buttonSearchMovies).Click();
+
+        
+        }
+    }
+}
