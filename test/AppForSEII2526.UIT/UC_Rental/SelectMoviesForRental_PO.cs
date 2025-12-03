1bea265f280071a0a498d7057c824358ee802ffc
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-12-03 12:43:15 +0100

    we add the title for the date From

--- test/AppForSEII2526.UIT/UC_Rental/SelectMoviesForRental_PO.cs
+++ test/AppForSEII2526.UIT/UC_Rental/SelectMoviesForRental_PO.cs
@@ -7,18 +7,22 @@ using OpenQA.Selenium.Support.UI;
 
 namespace AppForSEII2526.UIT.UC_Rental {
     public class SelectMoviesForRental_PO : PageObject {
-        By inputTitle = By.Id("movieTitle");
+        By inputTitle = By.Id("inputTitle");
         By inputGenre = By.Id("selectGenre");
         By buttonSearchMovies = By.Id("searchMovies");
+        By inputFrom = By.Id("fromDate");
         public SelectMoviesForRental_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) {
         }
-        public void SearchMovies(string title, string genre) {
+        public void SearchMovies(string title, string genre, string from) {
             //wait for the webelement to be clickable
             WaitForBeingClickable(inputTitle);
             _driver.FindElement(inputTitle).SendKeys(title);
             if (genre == "") genre = "All";
             SelectElement selectElement = new SelectElement(_driver.FindElement(inputGenre));
             selectElement.SelectByText(genre);
+
+            if (from!="") 
+                _driver.FindElement(inputFrom).SendKeys(from);
 
             _driver.FindElement(buttonSearchMovies).Click();
 
